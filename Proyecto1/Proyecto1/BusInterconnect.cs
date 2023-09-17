f4426e98ea8efbcf2de517f3094b71a71f9b9664
David De la Hoz <divad0907@gmail.com>
2023-09-17 15:03:49 -0600

    [feat]: add Businterconnect and Request class

---
+++ Proyecto1/Proyecto1/BusInterconnect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1
+{
+    public class BusInterconnect
+    {
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine(args.Length);
+        }
+
+        private int AddrBus {  get; set; }
+        private int DataBus {  get; set; }
+        private int SharedBus { get; set; }
+        private List<Request> queue {  get; set; }
+
+        private BusInterconnect() 
+        { 
+            this.queue = new List<Request>();
+        }
+
+    }
+
+    
+}
