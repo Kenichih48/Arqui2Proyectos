d7eb5358ee364345511a0c5d00d4536c74841638
David De la Hoz <divad0907@gmail.com>
2023-11-22 00:39:57 -0600

    [feat]: New Test Top

--- Proyecto_2/cpu/test/test_top.sv
+++ Proyecto_2/cpu/test/test_top.sv
@@ -4,11 +4,15 @@ module test_top();
     logic [31:0] WriteData, DataAdr;
     logic MemWrite, MemWriteVec;
 	 logic [31:0] DataAdrVec[0:3], WriteDataVec[0:3];
-	 
+	 logic H_SYNC, V_SYNC, SYNC_B, SYNC_BLANK;
+	 logic clk_25;
+    logic [7:0] r,g,b;
 	 
 
     // instantiate device to be tested
-    top dut(clk, reset, WriteData, DataAdr, DataAdrVec, WriteDataVec, MemWrite, MemWriteVec);
+    top dut(clk, reset, WriteData, DataAdr, 
+	 DataAdrVec, WriteDataVec, MemWrite, MemWriteVec,
+	 H_SYNC, V_SYNC, SYNC_B, SYNC_BLANK,clk_25,r,g,b);
 	 
 	 
     // initialize test
